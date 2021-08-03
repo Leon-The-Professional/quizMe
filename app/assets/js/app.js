eb460e81d37b1f3ff9f91c14931e83b43aefaa73
Leon Hall <leonus.hall@gmail.com>
2021-08-03 08:40:26 -0600

    add random question functionality using the Durstenfeld Shuffle algorithm

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -48,5 +48,47 @@ for (let i = 0; i < specificTabs.length; i++) {
     })
 }
 
+// Populating Quiz
+const quizContainer = document.querySelector('#quizContainer')
+const quiz = document.querySelector('#quiz')
+let currentQuiz = document.querySelector('.quizSelect')
+const getQuiz = document.querySelector('#getQuiz')
+// let randNum = function () { Math.random() * 10 }
+// Durstenfeld Shuffle 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+const populateQuiz = function (arr) {
+    quiz.innerHTML = ''
+    i = 1;
+    shuffleArray(arr);
+    for (let q of arr) {
+        let qContain = document.createElement('div')
+        qContain.classList.add('qcontain')
+        let question = document.createElement('p')
+        let number = document.createElement('span')
+        number.innerText = `${i}. `
+        question.innerText = number.innerText + q;
+        qContain.append(question);
+        question.classList.add('question')
+        let answer = document.createElement('textarea')
+        answer.setAttribute('rows', '5')
+        qContain.append(answer)
+        answer.classList.add('answer')
+        quiz.append(qContain)
+        i++;
+    }
+    let checkQuiz = document.createElement('button')
+    checkQuiz.innerText = 'Check Answers'
+    quiz.append(checkQuiz)
+}
 
+getQuiz.addEventListener('click', function () {
+    populateQuiz(LoopsQuiz)
+})
 
